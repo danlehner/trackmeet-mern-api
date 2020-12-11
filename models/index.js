1363837a6bc3c39beb51d69aec8e1f009c8e1706
danlehner <lehnerdr@gmail.com>
2020-12-11 17:13:02 -0500

    rename localhost for monogdb

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/trackmeet";
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/trackmeet-mern";
 
 mongoose
   .connect(connectionString, {
