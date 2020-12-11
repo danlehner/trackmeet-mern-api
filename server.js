b42a097c7ca66b4904f1a905ce306849c2e5ff37
danlehner <lehnerdr@gmail.com>
2020-12-11 16:25:05 -0500

    basic server fleshed out

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 // external exports 
 const express = require("express"); 
-const cors = require("cors")
+const cors = require("cors"); 
+
+// internal imports
+
+const PORT = process.env.PORT || 3001; 
+const app = express(); 
+
+// middleware JSON parsing
+app.use(express.json()); 
+app.use(cors()); 
+
+// middleware - API routes
+
+// Auth Routes
+
+// Users Routers
+
+// connection 
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
