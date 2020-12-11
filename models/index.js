c4abb17363c78098dc6ea7f1e272b539184a4e88
danlehner <lehnerdr@gmail.com>
2020-12-11 16:32:26 -0500

    adds basic set up for mongoose server

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/trackmeet";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(function () {
+    console.log("Mongodb connected....");
+  })
+  .catch(function (error) {
+    console.log("Mongodb connection err", error);
+  });
+
+mongoose.connection.on("disconnect", function (event) {
+  console.log("mongodb disconnected", event);
+});
+
+  module.exports = {
+    User: require('./User'),
+    Artist: require('./Artist'), 
+    Genre: require('./Genre'),
+    Song: require('./Song'),
+    User: require('./User')
+  }
